e63a2a64029400ef9f14fdee93e841093dd68a61
Jun Villa <jun@aucto.com>
2025-03-11 11:48:14 -0400

    add subtitles

--- src/App.tsx
+++ src/App.tsx
@@ -27,14 +27,14 @@ function App() {
   return (
     <>
       <section className="top-0 flex justify-center w-full h-16 border-b shadow-md bg-dusk-700/70 border-dusk-900 backdrop-blur-sm">
-        <div className="flex items-center w-full max-w-screen-xl px-2">
+        <div className="flex items-center w-full px-2 max-w-screen-2xl">
           <h1 className="w-full text-3xl">
             <span className="text-dusk-200">ani</span>mewing
           </h1>
         </div>
       </section>
 
-      <div className="grid w-full h-screen max-w-screen-xl grid-cols-[44px_1fr] lg:grid-cols-[140px_1fr]">
+      <div className="grid w-full h-screen max-w-screen-2xl grid-cols-[44px_1fr] lg:grid-cols-[140px_1fr]">
         {/* episode list */}
         <section className="overflow-y-auto border-r shadow-md border-dusk-800">
           <div className="hidden p-2 lg:block bg-black/50 line-clamp-1">Episode List</div>
@@ -66,15 +66,16 @@ function App() {
           <div className="relative">
             {/* subtitle overlay */}
             {showSubtitles && (
-              <div className="absolute z-10 flex flex-col items-center w-full gap-2 pointer-events-none bottom-8">
+              <div className="absolute z-10 flex flex-col items-center w-full gap-2 pointer-events-none bottom-4 md:bottom-8 2xl:bottom-14">
                 {currentSubtitles
                   ?.sort((a, b) => b.MarginV - a.MarginV)
                   ?.map((subtitle, i) => (
-                    <div
-                      className="pointer-events-auto text-center border-2 border-dusk-500 bg-black/70 text-white backdrop-blur-2xl px-6 py-4 text-3xl rounded-xl max-w-[80%] min-w-[55%]"
-                      key={i}
-                    >
-                      {subtitle.Text}
+                    <div className="flex items-center gap-4 max-w-[80%] min-w-[55%]" key={i}>
+                      {previousSubtitleHint}
+                      <div className="w-full px-3 py-2 text-center text-white border-2 pointer-events-auto md:px-6 md:py-4 2xl:py-8 text-md md:text-xl xl:text-3xl 2xl:text-4xl border-dusk-500 bg-black/70 backdrop-blur-2xl rounded-xl ">
+                        {subtitle.Text}
+                      </div>
+                      <div className="hidden w-16 lg:block" />
                     </div>
                   ))}
               </div>
@@ -88,7 +89,7 @@ function App() {
             <ul>
               <li>Space: Play/Pause</li>
               <li>Shift: Pause</li>
-              <li>Ctrl: Hide Subtitles</li>
+              <li>S: Hide Subtitles</li>
               <li>A: Go to previous subtitle</li>
               <li>D: Go to next subtitle</li>
             </ul>
@@ -98,5 +99,29 @@ function App() {
     </>
   );
 }
+
+const previousSubtitleHint = (
+  <div className="items-center hidden gap-2 lg:flex">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      fill="currentColor"
+      className="text-white drop-shadow-lg"
+      viewBox="0 0 16 16"
+    >
+      <path
+        fill-rule="evenodd"
+        d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8"
+      />
+    </svg>
+    <div
+      className="flex items-center justify-center w-8 py-1 font-mono text-sm font-bold border-2 border-dusk-400 bg-dusk-800"
+      style={{ boxShadow: "2px 2px #3b2a44 " }}
+    >
+      A
+    </div>
+  </div>
+);
 
 export default App;
--- src/hooks/keyboard-controls.ts
+++ src/hooks/keyboard-controls.ts
@@ -3,16 +3,16 @@ import useTranscript from "./transcript";
 
 export default function useKeyboardControls(
   videoRef: RefObject<HTMLVideoElement | null>,
-  transcript: ReturnType<typeof useTranscript>,
+  transcript: ReturnType<typeof useTranscript>
 ) {
   const [showSubtitles, setShowSubtitles] = useState(true);
   useEffect(() => {
     const keydown = (e: KeyboardEvent) => {
-      if (e.key === "Control") setShowSubtitles(false);
+      if (e.key === "s") setShowSubtitles(false);
       if (e.key === "Shift") videoRef.current?.pause();
     };
     const keyup = (e: KeyboardEvent) => {
-      if (e.key === "Control") setShowSubtitles(true);
+      if (e.key === "s") setShowSubtitles(true);
       if (e.key === "Shift") videoRef.current?.play();
     };
     const keypress = (e: KeyboardEvent) => {
@@ -26,10 +26,7 @@ export default function useKeyboardControls(
       } else if (e.key === "a") {
         const offset = 0.5;
         const previousDialogue = transcript
-          ?.filter(
-            (dialogue) =>
-              dialogue.Start < videoRef.current!.currentTime - offset || 0,
-          )
+          ?.filter((dialogue) => dialogue.Start < videoRef.current!.currentTime - offset || 0)
           ?.sort((a, b) => b.Start - a.Start)[0];
         if (previousDialogue) {
           videoRef.current!.currentTime = previousDialogue.Start;
@@ -37,9 +34,7 @@ export default function useKeyboardControls(
         }
       } else if (e.key === "d") {
         const nextDialogue = transcript
-          ?.filter(
-            (dialogue) => dialogue.Start > videoRef.current!.currentTime || 0,
-          )
+          ?.filter((dialogue) => dialogue.Start > videoRef.current!.currentTime || 0)
           ?.sort((a, b) => a.Start - b.Start)[0];
         if (nextDialogue) {
           videoRef.current!.currentTime = nextDialogue.Start;
